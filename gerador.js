58389ef8343936803376537da906221f88798ab4
alexxfaria <alexxfs@live.com>
2021-06-28 12:37:21 -0300

    Add files via upload

---
+++ gerador.js
@@ -0,0 +1,17 @@
+var lbcomum = document.getElementById('lbcomum');
+
+function comumClick(){
+  lbcomum.innerHTML = 'C'
+};
+
+function rapidoClick(){};
+
+function prioritarioClick(){};
+
+function caixa1lick(){};
+
+function caixa2Click(){};
+
+function caixa3Click(){};
+
+function caixa4Click(){};
